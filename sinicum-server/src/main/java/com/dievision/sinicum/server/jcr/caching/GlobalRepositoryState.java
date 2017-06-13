f1f4adddbe05b750e0b4d9c0118967968d218875
Daniel Trierweiler <trierweiler@dievision.de>
2017-06-13 17:40:30 +0200

    Introducing a new cache key, that is scoped for site prefixes. Needed in a multisite environment.

--- sinicum-server/src/main/java/com/dievision/sinicum/server/jcr/caching/GlobalRepositoryState.java
+++ sinicum-server/src/main/java/com/dievision/sinicum/server/jcr/caching/GlobalRepositoryState.java
@@ -4,8 +4,11 @@ import org.apache.commons.codec.digest.DigestUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
+
 public final class GlobalRepositoryState {
     private static String cacheKey;
+    private static HashMap<String, String> cacheKeys = new HashMap<String, String>();
     private static final Logger logger = LoggerFactory.getLogger(GlobalRepositoryState.class);
 
     private GlobalRepositoryState() {
@@ -14,13 +17,25 @@ public final class GlobalRepositoryState {
 
     public static String getCacheKey() {
         if (cacheKey == null) {
-            updateCacheKey();
+            GlobalRepositoryState.updateCacheKey();
         }
         return cacheKey;
+    }
+
+    public static String getCacheKey(String sitePrefix) {
+        if (!cacheKeys.containsKey(sitePrefix)) {
+            GlobalRepositoryState.updateCacheKey(sitePrefix);
+        }
+        return cacheKeys.get(sitePrefix);
     }
 
     public static void updateCacheKey() {
         String time = Long.toString(System.currentTimeMillis());
         cacheKey = DigestUtils.shaHex(time);
+    }
+
+    public static void updateCacheKey(String sitePrefix) {
+        String time = Long.toString(System.currentTimeMillis());
+        cacheKeys.put(sitePrefix, DigestUtils.shaHex(sitePrefix + time));
     }
 }
